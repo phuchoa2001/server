d2b6275a833b5b29db8e01862cef723d79c8b920
phuchoa <phuchoa1202@gmail.com>
2021-12-25 21:03:51 +0700

    A

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const { Server } = require("socket.io");
 const socket = require("./socket")
 const io = new Server(server, {
   cors: {
-    origin: "http://xoanen.surge.sh",
+    origin: "xoanen.surge.sh",
     methods: ["GET", "POST" ,"PUT" , "PATCH" , "HEAD" , "DELETE"]
   }, 
 });
